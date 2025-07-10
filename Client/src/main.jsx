228b0d24d1f7cf78b8313a67c9cce01fec297f65
MD. RAKIBUL HASAN HEMEL <rhn109820@gmail.com>
2025-07-10 23:21:18 +0600

    Basic Setup

---
+++ Client/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { BrowserRouter } from "react-router";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+);
