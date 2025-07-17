98ba6dae6274cd1be9c59b3cc76dcb46cce9bf9c
MD. RAKIBUL HASAN HEMEL <rhn109820@gmail.com>
2025-07-17 13:28:10 +0600

    Extention is Important

--- Server/src/routes/singleUserRoutes.js
+++ Server/src/routes/singleUserRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser } from "../controllers/userController";
+import { getUser } from "../controllers/userController.js";
 
 const router = express.Router();
 router.get("/me/:id", getUser)
