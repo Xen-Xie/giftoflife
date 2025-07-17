8718574405c0322e3d906f0eef765f3fa9fa92cd
MD. RAKIBUL HASAN HEMEL <rhn109820@gmail.com>
2025-07-17 16:02:52 +0600

    Path Fix

--- Server/src/routes/singleUserRoutes.js
+++ Server/src/routes/singleUserRoutes.js
@@ -3,6 +3,6 @@ import { addDonationDate, getUser } from "../controllers/userController.js";
 
 const router = express.Router();
 router.get("/me/:id", getUser)
-router.patch("/me/:id/add-donation", addDonationDate)
+router.patch("/me/add-donation/:id", addDonationDate)
 
 export default router;
